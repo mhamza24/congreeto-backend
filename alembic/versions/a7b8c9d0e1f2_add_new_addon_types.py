680155acfaf66cb57b3d933ce396aa5e56432807
mhamza24 <muhammadhamzakhalid24@gmail.com>
2026-04-15 01:08:35 +0500

    feat:fix addon migration

--- alembic/versions/a7b8c9d0e1f2_add_new_addon_types.py
+++ alembic/versions/a7b8c9d0e1f2_add_new_addon_types.py
@@ -1,6 +1,6 @@
 """add extra_tokens, extra_chatbots, custom_banner, extra_ribbon_messages to addon_type
 
-Revision ID: f1a2b3c4d5e6
+Revision ID: a7b8c9d0e1f2
 Revises: e6f7a8b9c0d1
 Create Date: 2026-04-15
 
@@ -12,7 +12,7 @@ from typing import Sequence, Union
 from alembic import op
 
 
-revision: str = 'f1a2b3c4d5e6'
+revision: str = 'a7b8c9d0e1f2'
 down_revision: Union[str, None] = 'e6f7a8b9c0d1'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
