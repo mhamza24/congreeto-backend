0839eebe0280d75edeb676ff7b73fc93d75bedea
mhamza24 <muhammadhamzakhalid24@gmail.com>
2026-04-15 02:47:04 +0500

    feat:added frontend url and fix main run

--- main.py
+++ main.py
@@ -109,4 +109,6 @@ app.include_router(v1_router, prefix="/api/v1")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
